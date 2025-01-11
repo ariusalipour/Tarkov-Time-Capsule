f9145cebb7cbff3fcd18a6942e34151705dcafbe
Aryan Alipour <aryan.alipour@outlook.com>
2025-01-11 03:10:06 +0000

    Add date to discord bot response

--- tarkov-time-capsule-discord/src/handler/bossHandler.ts
+++ tarkov-time-capsule-discord/src/handler/bossHandler.ts
@@ -89,11 +89,4 @@ export const bossHandler: InteractionHandler = async (
 		};
 	}
 
-	function formatDate(date: Date): string {
-		const day = String(date.getDate()).padStart(2, '0');
-		const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-		const year = date.getFullYear();
-		return `${day}/${month}/${year}`;
-	}
-
 };
